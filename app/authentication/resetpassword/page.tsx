4b82c023121ad005db344cb8cf0990573ecf273a
ganeshsadhashivam <ganeshsadhashivam@outlook.com>
2024-12-02 23:32:35 +0530

    force-dynamic in resetpassword

--- app/authentication/resetpassword/page.tsx
+++ app/authentication/resetpassword/page.tsx
@@ -5,6 +5,8 @@ import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+export const dynamic = "force-dynamic";
+
 export default function ResetPasswordPage() {
   const searchParams = useSearchParams();
   const token = searchParams.get("token");
