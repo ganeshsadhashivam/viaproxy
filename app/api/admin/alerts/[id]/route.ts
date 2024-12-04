ef4a7ca01c4716b875b2719a20671bd4bb6c8819
ganeshsadhashivam <ganeshsadhashivam@outlook.com>
2024-12-04 23:49:25 +0530

    packages installed formik,antd,formik-antd

--- app/api/admin/alerts/[id]/route.ts
+++ app/api/admin/alerts/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-import connectToDatabase from "@/lib/dbconfig/db";
-import { Alert } from "@/models/adminmodels/Alert";
+import connectToDatabase from "@/utils/dbConnect";
+import { Alert } from "@/models/admin/Alert";
 
 export async function DELETE(
   request: NextRequest,
