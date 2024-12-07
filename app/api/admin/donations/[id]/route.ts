fe219ff0eb7044b9e1ccf1f689f463be21df98f6
ganeshsadhashivam <ganeshsadhashivam@outlook.com>
2024-12-07 13:15:54 +0530

    patch request in admin donation

--- app/api/admin/donations/[id]/route.ts
+++ app/api/admin/donations/[id]/route.ts
@@ -1,21 +1,23 @@
-import { NextRequest, NextResponse } from "next/server";
-import connectToDatabase from "@/utils/dbConnect";
-import { Donation } from "@/models/admin/Donation";
+export async function PATCH() {}
 
-export async function PATCH(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  await connectToDatabase();
-  try {
-    const { status } = await request.json();
-    const donation = await Donation.findByIdAndUpdate(
-      params.id,
-      { status },
-      { new: true }
-    );
-    return NextResponse.json(donation, { status: 200 });
-  } catch (error) {
-    return NextResponse.json({ error: error }, { status: 500 });
-  }
-}
+// import { NextRequest, NextResponse } from "next/server";
+// import connectToDatabase from "@/utils/dbConnect";
+// import { Donation } from "@/models/admin/Donation";
+
+// export async function PATCH(
+//   request: NextRequest,
+//   { params }: { params: { id: string } }
+// ) {
+//   await connectToDatabase();
+//   try {
+//     const { status } = await request.json();
+//     const donation = await Donation.findByIdAndUpdate(
+//       params.id,
+//       { status },
+//       { new: true }
+//     );
+//     return NextResponse.json(donation, { status: 200 });
+//   } catch (error) {
+//     return NextResponse.json({ error: error }, { status: 500 });
+//   }
+// }
