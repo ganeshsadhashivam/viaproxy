c5285304883e47db7629609d8ae6865f3fa95fc3
ganeshsadhashivam <ganeshsadhashivam@outlook.com>
2024-12-07 13:22:07 +0530

    delete api in message

--- app/api/admin/messages/[id]/route.ts
+++ app/api/admin/messages/[id]/route.ts
@@ -1,19 +1,21 @@
-import { NextRequest, NextResponse } from "next/server";
-import connectToDatabase from "@/utils/dbConnect";
-import { Message } from "@/models/admin/Message";
+export async function DELETE() {}
 
-export async function DELETE(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  await connectToDatabase();
-  try {
-    await Message.findByIdAndDelete(params.id);
-    return NextResponse.json(
-      { message: "Message deleted successfully" },
-      { status: 200 }
-    );
-  } catch (error) {
-    return NextResponse.json({ error: error }, { status: 500 });
-  }
-}
+// import { NextRequest, NextResponse } from "next/server";
+// import connectToDatabase from "@/utils/dbConnect";
+// import { Message } from "@/models/admin/Message";
+
+// export async function DELETE(
+//   request: NextRequest,
+//   { params }: { params: { id: string } }
+// ) {
+//   await connectToDatabase();
+//   try {
+//     await Message.findByIdAndDelete(params.id);
+//     return NextResponse.json(
+//       { message: "Message deleted successfully" },
+//       { status: 200 }
+//     );
+//   } catch (error) {
+//     return NextResponse.json({ error: error }, { status: 500 });
+//   }
+// }
