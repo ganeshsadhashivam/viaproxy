cfdde35d3ed5fc73c512112a4bf0e22f78cb06bf
ganeshsadhashivam <ganeshsadhashivam@outlook.com>
2024-12-07 13:18:33 +0530

    api admin mediation patch

--- app/api/admin/mediation/[id]/route.ts
+++ app/api/admin/mediation/[id]/route.ts
@@ -1,21 +1,23 @@
-import { NextRequest, NextResponse } from "next/server";
-import connectToDatabase from "@/utils/dbConnect";
-import { Mediation } from "@/models/admin/Mediation";
+export async function PATCH() {}
 
-export async function PATCH(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  await connectToDatabase();
-  try {
-    const { resolution, status } = await request.json();
-    const mediation = await Mediation.findByIdAndUpdate(
-      params.id,
-      { resolution, status },
-      { new: true }
-    );
-    return NextResponse.json(mediation, { status: 200 });
-  } catch (error) {
-    return NextResponse.json({ error: error }, { status: 500 });
-  }
-}
+// import { NextRequest, NextResponse } from "next/server";
+// import connectToDatabase from "@/utils/dbConnect";
+// import { Mediation } from "@/models/admin/Mediation";
+
+// export async function PATCH(
+//   request: NextRequest,
+//   { params }: { params: { id: string } }
+// ) {
+//   await connectToDatabase();
+//   try {
+//     const { resolution, status } = await request.json();
+//     const mediation = await Mediation.findByIdAndUpdate(
+//       params.id,
+//       { resolution, status },
+//       { new: true }
+//     );
+//     return NextResponse.json(mediation, { status: 200 });
+//   } catch (error) {
+//     return NextResponse.json({ error: error }, { status: 500 });
+//   }
+// }
