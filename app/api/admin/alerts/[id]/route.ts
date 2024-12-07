2b09d90d9d97cf9629f3aba9938e24180d06cba2
ganeshsadhashivam <ganeshsadhashivam@outlook.com>
2024-12-07 12:36:03 +0530

    modified api admin id  delete route

--- app/api/admin/alerts/[id]/route.ts
+++ app/api/admin/alerts/[id]/route.ts
@@ -4,8 +4,9 @@ import { Alert } from "@/models/admin/Alert";
 
 export async function DELETE(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  context: { params: { id: string } }
 ) {
+  const { params } = context; // Extract params from context
   await connectToDatabase();
   try {
     await Alert.findByIdAndDelete(params.id);
@@ -14,6 +15,29 @@ export async function DELETE(
       { status: 200 }
     );
   } catch (error) {
-    return NextResponse.json({ error: error }, { status: 500 });
+    return NextResponse.json(
+      { error: error || "An error occurred" },
+      { status: 500 }
+    );
   }
 }
+
+// import { NextRequest, NextResponse } from "next/server";
+// import connectToDatabase from "@/utils/dbConnect";
+// import { Alert } from "@/models/admin/Alert";
+
+// export async function DELETE(
+//   request: NextRequest,
+//   { params }: { params: { id: string } }
+// ) {
+//   await connectToDatabase();
+//   try {
+//     await Alert.findByIdAndDelete(params.id);
+//     return NextResponse.json(
+//       { message: "Alert dismissed successfully" },
+//       { status: 200 }
+//     );
+//   } catch (error) {
+//     return NextResponse.json({ error: error }, { status: 500 });
+//   }
+// }
