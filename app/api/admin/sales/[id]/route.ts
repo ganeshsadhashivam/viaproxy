bab3e48c0ead28590867be5c879f57d587ad49ef
ganeshsadhashivam <ganeshsadhashivam@outlook.com>
2024-12-07 13:25:17 +0530

    app/api/admin/sales/[id]/route.ts" has an invalid "PATCH"

--- app/api/admin/sales/[id]/route.ts
+++ app/api/admin/sales/[id]/route.ts
@@ -1,21 +1,22 @@
-import { NextRequest, NextResponse } from "next/server";
-import connectToDatabase from "@/utils/dbConnect";
-import { Sale } from "@/models/admin/Sale";
+export default function PATCH() {}
+// import { NextRequest, NextResponse } from "next/server";
+// import connectToDatabase from "@/utils/dbConnect";
+// import { Sale } from "@/models/admin/Sale";
 
-export async function PATCH(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  await connectToDatabase();
-  try {
-    const { status } = await request.json();
-    const sale = await Sale.findByIdAndUpdate(
-      params.id,
-      { status },
-      { new: true }
-    );
-    return NextResponse.json(sale, { status: 200 });
-  } catch (error) {
-    return NextResponse.json({ error: error }, { status: 500 });
-  }
-}
+// export async function PATCH(
+//   request: NextRequest,
+//   { params }: { params: { id: string } }
+// ) {
+//   await connectToDatabase();
+//   try {
+//     const { status } = await request.json();
+//     const sale = await Sale.findByIdAndUpdate(
+//       params.id,
+//       { status },
+//       { new: true }
+//     );
+//     return NextResponse.json(sale, { status: 200 });
+//   } catch (error) {
+//     return NextResponse.json({ error: error }, { status: 500 });
+//   }
+// }
